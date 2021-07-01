191411fb7f1e3eeb40650f66ec4ccbdca23a65ea
Việt Khuất <vietviet2111st@example.com>
2021-07-01 16:59:49 +0700

    done

---
+++ js/myJs.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function firstQuestion(){
+    
+    $('.content').hide();
+    Swal.fire({
+        title:  'He luu đã lâu ko gặp!',
+        text: 'Anh có điều này muốn hỏi em nhớ phải trả lời nhaaa.',
+        imageUrl: 'https://scontent-hkg4-2.xx.fbcdn.net/v/t1.15752-9/208526024_918829422009687_8195323026289130448_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=ae9488&_nc_ohc=F0BdDr7d2uMAX_HW0gf&tn=sGjxJ4g_ksRjAyMd&_nc_ht=scontent-hkg4-2.xx&oh=fef72745fa0213cf62aed7ab8cb23c35&oe=60E2DCA5',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+      }).then(function(){
+        $('.content').show(200);
+      })
+}
+
+ // switch button position
+ function switchButton() {
+    var audio = new Audio('sound/duck.mp3');
+    audio.play();
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+// move random button póition
+function moveButton() {
+    var audio = new Audio('sound/Swish1.mp3');
+    audio.play();
+    if (screen.width<=600) {
+        var x = Math.random() * 300;
+        var y = Math.random() * 500;
+    } else{
+        var x = Math.random() * 500;
+        var y = Math.random() * 500;
+    }
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+
+var n = 0;
+$('#no').mousemove(function() {
+    if (n < 1)
+        switchButton();
+    if (n > 1)
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    if (screen.width>=900)
+        switchButton();
+})
+
+// generate text in input
+function textGenerate() {
+    var n = "";
+    var text = "Tại vì anh cute vl :<<<<<<< ";
+    var a = Array.from(text);
+    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
+    $('#txtReason').val(n);
+    setTimeout("textGenerate()", 1);
+}
+
+// show popup
+$('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    Swal.fire({
+        title: 'Nói cho Anh lí do em thích Anh đi :vvvv',
+        html: true,
+        width: 900,
+        padding: '3em',
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+        background: '#fff url("img/iput-bg.jpg")',
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/giphy2.gif")
+              left top
+              no-repeat
+            `,
+        showCancelButton: true,
+        cancelButtonText: "Thôi ngại lém :<<",
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#fe8a71',
+        cancelButtonColor: '#f6cd61',
+        confirmButtonText: 'Gửi cho tớ <3'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                width: 900,
+                confirmButtonText: 'Okiiiii lun <3',
+                background: '#fff url("img/iput-bg.jpg")',
+                title: 'Anh biết mà ^^ thích em hơi nhiều',
+                text: "Tối nay Anh qua đón em đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho Anh đi nàoooooo",
+                confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = 'https://www.facebook.com/messages/t/100007176723353/';
+                  }
+            })
+        }
+    })
+})
